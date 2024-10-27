734e8e57cec3577a67330303a6bf7b2273237b47
Mark Taiwan <marktaiwan@users.noreply.github.com>
2024-10-27 23:25:58 +0800

    Version 1.1.5

--- eh-enhanced-keyboard-navigation.user.js
+++ eh-enhanced-keyboard-navigation.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         EH Enhanced Keyboard Navigation
 // @description  Configurable shortcuts and enhanced keyboard navigation. "Ctrl+Shift+/" to open settings.
-// @version      1.1.4
+// @version      1.1.5
 // @author       Marker
 // @license      MIT
 // @namespace    https://github.com/marktaiwan/
